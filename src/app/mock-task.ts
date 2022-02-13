303d7ec1cd6fc90f8557d8f8f48a99be59654519
BAR-24 <alexisrodrigobastias@gmail.com>
2022-02-12 22:09:44 -0300

    Agregamos mock de tareas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,21 @@
+exports const TASK = [
+    {
+        id : 1,
+        text: "Terminar primer App de angular",
+        day: '20 de febrero 2022',
+        reminder: true  
+    },
+    {
+        id : 2,
+        text: "Empezar modulo 4",
+        day: '21 de febrero 2022',
+        reminder: true    
+    },
+    {
+        id : 3,
+        text: "Salir a paser",
+        day: '28 de febrero 2022',
+        reminder: false  
+    }
+    
+]
